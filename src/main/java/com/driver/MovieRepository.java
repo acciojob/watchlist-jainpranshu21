9ecb00a1fc814e63b98fb1f7038cbefc70a59d95
jainpranshu21 <pranshujain80@gmail.com>
2023-05-25 11:55:58 +0530

    watchlist

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -13,11 +13,7 @@ public class MovieRepository {
     HashMap<String,Director>directorDb=new HashMap<>();
     HashMap<String,String>movieDirectorDb=new HashMap<>();
 
-    /*public MovieRepository() {
-        this.movieDb = new HashMap<String, Movie>();
-        this.directorDb = new HashMap<String, Director>();
-        this.movieDirectorDb = new HashMap<String,String>();
-    }*/
+
 
     public String addMovie(Movie movie){
       String name=movie.getName();
